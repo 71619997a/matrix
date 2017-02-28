05b3273b72e898f3445fa4c68ae586e0d147c89b
71619997a <marksg07@gmail.com>
2017-02-28 11:21:36 -0500

    Edge matrix 4xn not nx4

--- edgeMtx.py
+++ edgeMtx.py
@@ -2,9 +2,14 @@ from base import Image
 from line import line
 import matrix
 
-def addPoint(m, x, y, z):
-    m.append([x, y, z, 1.])
+def edgemtx():
+    return [[],[],[],[]]
 
+def addPoint(m, x, y, z):
+    m[0].append(x)
+    m[1].append(y)
+    m[2].append(z)
+    m[3].append(1.)
 
 def addEdge(m, x0, y0, z0, x1, y1, z1):
     addPoint(m, x0, y0, z0)
@@ -12,8 +17,8 @@ def addEdge(m, x0, y0, z0, x1, y1, z1):
 
 
 def drawEdges(m, image, color=(255, 0, 0)):  # draws the edges to an image
-    for i in range(0, len(m) - 1, 2):
-        lin = line(m[i][0], m[i][1], m[i + 1][0], m[i + 1][1])
+    for i in range(0, len(m[0]) - 1, 2):
+        lin = line(m[0][i], m[1][i], m[0][i + 1], m[1][i + 1])
         coloredlin = [xy + (color,) for xy in lin]
         image.setPixels(coloredlin)
 
@@ -55,10 +60,14 @@ if __name__ == '__main__':
     print matrix.toStr(matrix.multiply(m21, 10))
     print '10 * 10'
     print matrix.multiply(10, 10)
+    print 'Adding edge (1, 1, 1), (2, 3, 2.5)'
+    m = edgemtx()
+    addEdge(m, 1, 1, 1, 2, 3, 2.5)
+    print matrix.toStr(m)
     
     img = Image(500, 500)
     for loc in range(0, 500, 4):
-        edges = []
+        edges = edgemtx()
         addEdge(edges, 125, loc, 0, loc + 1, 375, 0)
         addEdge(edges, loc + 1, 375, 0, 375, 500 - loc - 2, 0)
         addEdge(edges, 375, 500 - loc - 2, 0, 500 - loc - 3, 125, 0)
